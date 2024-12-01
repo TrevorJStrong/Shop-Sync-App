3e304a5c1ccd40c5de210b3f11958d7aed12e220
Trevor Strong <36681787+TrevorJStrong@users.noreply.github.com>
2024-12-01 19:07:57 +0000

    feat: react-native-onesignal added

--- ShopSync/App.tsx
+++ ShopSync/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {QueryClientProvider} from '@tanstack/react-query';
-// import {LogLevel, OneSignal} from 'react-native-onesignal';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
+import Constants from 'expo-constants';
 
 import {queryClient} from './queryClient';
 import RootStack from './src/navigation';
@@ -17,19 +18,19 @@ import {useAuthStore} from './src/hooks/useStore';
 
 const App = () => {
   // Remove this method to stop OneSignal Debugging
-  // OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
 
   // OneSignal Initialization
-  // OneSignal.initialize(Config.ONESIGNAL_APP_ID ?? '');
+  OneSignal.initialize(Constants?.expoConfig?.extra?.oneSignalAppId);
 
   // requestPermission will show the native iOS or Android notification permission prompt.
   // We recommend removing the following code and instead using an In-App Message to prompt for notification permission
-  // OneSignal.Notifications.requestPermission(true);
+  OneSignal.Notifications.requestPermission(true);
 
   // Method for listening for notification clicks
-  // OneSignal.Notifications.addEventListener('click', event => {
-  //   console.log('OneSignal: notification clicked:', event);
-  // });
+  OneSignal.Notifications.addEventListener('click', event => {
+    console.log('OneSignal: notification clicked:', event);
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
